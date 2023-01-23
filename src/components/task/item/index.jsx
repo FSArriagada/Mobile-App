19aed4966f3bd283b7034a129d8a6e0f1277bb3d
unknown <facu._99@hotmail.com>
2023-01-23 20:42:45 -0300

    Adding checkbox

--- src/components/task/item/index.jsx
+++ src/components/task/item/index.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {TouchableOpacity, Text} from 'react-native';
 import { styles } from "./styles";
 
 const TaskItem = ({item, onHandlerModal}) => {
+    const [options,setOptions] = useState([]);
+
+    function check (selectedOption){
+    
+        if (options.includes(selectedOption)){
+            setOptions(options.filter (option => option !== selectedOption))
+            return;
+        }
+
+        setOptions(options => options.concat(selectedOption))
+    }
+    
     return(
         <TouchableOpacity style={styles.itemContainer} onPress={() => onHandlerModal(item)}>
+            {options.map (option => (
+                <TouchableOpacity style={styles.checkbox} onPress={()=>check(option)}>
+                    {options.includes(option) && <Text>✔</Text>}
+                </TouchableOpacity>
+            ))}
+            
             <Text style={styles.itemList}>{item.value}</Text>
         </TouchableOpacity>
     )
--- src/components/task/item/styles.js
+++ src/components/task/item/styles.js
@@ -15,5 +15,15 @@ itemList: {
     backgroundColor:'#66256B',
     marginBottom: 10,
     borderRadius: 10
+  },
+  checkbox: {
+    width:25,
+    height:25,
+    borderWidth:2,
+    borderColor:'#1A183F',
+    marginRight:5,
+  },
+  check: {
+    alignSelf:'center',
   }
 })
